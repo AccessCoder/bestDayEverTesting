ac6b555d715225b9a7b6efbdb16d5411701a919b
AccessCoder <info.unaddict@gmail.com>
2026-07-03 15:03:17 +0200

    add tdd example

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,35 @@ public class Main {
 
     }
 
+    /**
+     * Spielregeln FizzBuzz
+     * Wir geben die übergebene Zahl zurück, ausser in folgenden Fällen:
+     * 1. Ist eine Zahl durch drei teilbar? -> Ausgabe "Fizz"
+     * 2. Ist eine Zahl durch fünf teilbar? -> Ausgabe "Buzz"
+     * 3. Ist eine Zahl durch drei und fünf teilbar? -> Ausgabe "FizzBuzz"
+     *
+     */
+
+    public static String fizzBuzz(int number){
+        if (number % 3 ==0 && number % 5==0){
+            return "FizzBuzz";
+        } else if(number % 5==0){
+            return "Buzz";
+        } else if (number % 3 ==0) {
+            return  "Fizz";
+        }
+        return ""+number;
+    }
+
+
+
+
+
+
+
+
+
+
 
     public static int add(int a, int b) {
         return a+b;
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -4,31 +4,130 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
+//    @Test
+//    void add_shouldReturn7_whenCalledWith5And2() {
+//        //GIVEN -> Vorbereitung -> Was weiß/brauche ich alles?
+//        int a = 5;
+//        int b = 2;
+//        int expected = 7;
+//        //WHEN -> Methodenaufruf -> Reality Check
+//        int actual = Main.add(a, b);
+//        //THEN -> Vergleich -> Erwartung vs Realität
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    void isEven_shouldReturnTrue_whenCalledWith2() {
+//        //GIVEN
+//        int a = 2;
+//        //WHEN
+//        boolean actual = Main.isEven(a);
+//        //THEN
+//        assertTrue(actual);
+//    }
+//
+//    @Test
+//    void isEven_shouldReturnFalse_whenCalledWith1() {
+//      assertFalse(Main.isEven(1));
+//    }
+
+
     @Test
-    void add_shouldReturn7_whenCalledWith5And2() {
-        //GIVEN -> Vorbereitung -> Was weiß/brauche ich alles?
-        int a = 5;
-        int b = 2;
-        int expected = 7;
-        //WHEN -> Methodenaufruf -> Reality Check
-        int actual = Main.add(a, b);
-        //THEN -> Vergleich -> Erwartung vs Realität
+    void fizzBuzz_shouldReturn1_whenGiven1(){
+        //GIVEN
+        int n = 1;
+        String expected = "1";
+        //WHEN
+        String actual = Main.fizzBuzz(n);
+        //THEN
         assertEquals(expected, actual);
     }
 
     @Test
-    void isEven_shouldReturnTrue_whenCalledWith2() {
+    void fizzBuzz_shouldReturn2_whenGiven2(){
         //GIVEN
-        int a = 2;
+        int n = 2;
+        String expected = "2";
         //WHEN
-        boolean actual = Main.isEven(a);
+        String actual = Main.fizzBuzz(n);
         //THEN
-        assertTrue(actual);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void isEven_shouldReturnFalse_whenCalledWith1() {
-      assertFalse(Main.isEven(1));
+    void fizzBuzz_shouldReturnFizz_whenGiven3(){
+        //GIVEN
+        int n = 3;
+        String expected = "Fizz";
+        //WHEN
+        String actual = Main.fizzBuzz(n);
+        //THEN
+        assertEquals(expected, actual);
     }
+
+    @Test
+    void fizzBuzz_shouldReturnFizz_whenGiven6(){
+        //GIVEN
+        int n = 6;
+        String expected = "Fizz";
+        //WHEN
+        String actual = Main.fizzBuzz(n);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void fizzBuzz_shouldReturnBuzz_whenGiven5(){
+        //GIVEN
+        int n = 5;
+        String expected = "Buzz";
+        //WHEN
+        String actual = Main.fizzBuzz(n);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void fizzBuzz_shouldReturnFizzBuzz_whenGiven15(){
+        //GIVEN
+        int n = 15;
+        String expected = "FizzBuzz";
+        //WHEN
+        String actual = Main.fizzBuzz(n);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void fizzBuzz_shouldReturnFizzBuzz_whenGiven0(){
+        //GIVEN
+        int n = 0;
+        String expected = "FizzBuzz";
+        //WHEN
+        String actual = Main.fizzBuzz(n);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void fizzBuzz_shouldReturnFizz_whenGivenMinus3(){
+        //GIVEN
+        int n = -3;
+        String expected = "Fizz";
+        //WHEN
+        String actual = Main.fizzBuzz(n);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+
+
+
+
+
+
+
+
+
 
 }
